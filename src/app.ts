bf14c50ef11b18a7fd62f8f8aac7e1d2730b9719
shuaib-code <shuaib.cyclist@gmail.com>
2025-04-19 00:41:59 +0600

    Implemented SSLcommerz Payment system

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,17 @@
-import express, { Application, Request, Response } from 'express'
-import cors from 'cors'
-import router from './app/router'
-import globalErrorHandler from './app/middleware/globalErrorHandler'
-import notFound from './app/error/notFound'
 import cookieParser from 'cookie-parser'
+import cors from 'cors'
+import express, { Application, Request, Response } from 'express'
+import notFound from './app/error/notFound'
+import globalErrorHandler from './app/middleware/globalErrorHandler'
+import router from './app/router'
 const app: Application = express()
 
 // middleware
+app.use(express.urlencoded({ extended: true })) // to parse SSLCommerz data
 app.use(express.json())
 app.use(
   cors({
-    origin: '*', 
+    origin: '*',
     credentials: true,
   })
 )
